e4a5b8f0d2618d74b3b7161427e2e9bfab872fe2
Kaiwang Chen <kaiwang.chen@gmail.com>
2020-12-06 01:43:08 +0800

    Day 2 - setState

--- main.js
+++ main.js
@@ -11,8 +11,10 @@ class MyComponent extends Component {
     render() {
         return <div>
             <h1>my component</h1>
-            <button onclick={() => {this.state.a++; this.rerender();}}>add</button>
-            <span>{ this.state.a.toString()}</span>
+            <button onclick={() => { this.setState({a: this.state.a + 1 }) }}>add</button>
+            <hr/>
+            <span>{ this.state.a.toString()}</span><br/>
+            <span>{ this.state.b.toString()}</span>
         </div>;
     }
 }
