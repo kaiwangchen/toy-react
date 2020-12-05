25431313fc014b313ab850c625ad507ba16718f0
Kaiwang Chen <kaiwang.chen@gmail.com>
2020-12-06 01:29:36 +0800

    Day 2 - rerender

--- toy-react.js
+++ toy-react.js
@@ -6,7 +6,13 @@ class ElementWrapper {
     }
 
     setAttribute(name, value) {
-        this.root.setAttribute(name, value);
+        if (name.match(/^on([\s\S]+)/)) {
+            this.root.addEventListener(
+                RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()),
+                value);
+        } else {
+            this.root.setAttribute(name, value);
+        }
     }
 
     appendChild(component) {
@@ -49,7 +55,13 @@ export class Component {
     }
 
     [RENDER_TO_DOM](range) {
+        this._range = range;
         this.render()[RENDER_TO_DOM](range);
+    }
+
+    rerender() {
+        this._range.deleteContents();
+        this[RENDER_TO_DOM](this._range);
     }
 }
 
